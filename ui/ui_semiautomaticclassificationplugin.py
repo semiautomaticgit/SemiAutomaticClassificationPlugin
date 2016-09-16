24f9a41477a07f6f8c8f46595b92fb2d2f5b7fd5
semiautomaticgit <semiautomatic.blogger@gmail.com>
2016-09-16 14:43:00 +0200

    5.0.8

--- ui/ui_semiautomaticclassificationplugin.py
+++ ui/ui_semiautomaticclassificationplugin.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/ui_semiautomaticclassificationplugin.ui'
 #
-# Created: Fri Sep 16 14:34:25 2016
+# Created: Fri Sep 16 14:41:03 2016
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
